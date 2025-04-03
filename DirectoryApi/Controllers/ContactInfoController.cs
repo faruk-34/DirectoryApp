c6f6b1a10a74e9790ad6010df8d6edd22593a0e2
FK <FK@FARUKKALKAN>
2025-04-03 18:07:40 +0300

    ContactInfo crud operations completed

---
+++ DirectoryApi/Controllers/ContactInfoController.cs
@@ -0,0 +1,36 @@
+﻿using DirectoryApi.Application.Interfaces;
+using DirectoryApi.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Directory = DirectoryApi.Domain.Entities.Directory;
+
+
+namespace DirectoryApi.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactInfoController : ControllerBase
+    {
+        private readonly IContactInfoService _contactInfoService;
+
+        public ContactInfoController(IContactInfoService contactInfoService)
+        {
+            _contactInfoService = contactInfoService;
+        }
+
+        [HttpPost]
+        public async Task<Directory> Insert(ContactInfo request)
+        {
+
+            var result = await _contactInfoService.Insert(request);
+            return result;
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _contactInfoService.Delete(id);
+            return NoContent();
+        }
+    }
+}
+
